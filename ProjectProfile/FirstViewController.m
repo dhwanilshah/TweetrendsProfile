5cb97c27e2729ca306e8486f4b66c367c77020f5
dhwanilshah <dcs426@nyu.edu>
2014-03-02 15:14:57 -0500

    Initial Commit

---
+++ ProjectProfile/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  ProjectProfile
+//
+//  Created by Dhwanil Shah on 3/2/14.
+//  Copyright (c) 2014 Dhwanil Shah. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ProjectProfile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectProfile
+//
+//  Created by Dhwanil Shah on 3/2/14.
+//  Copyright (c) 2014 Dhwanil Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
